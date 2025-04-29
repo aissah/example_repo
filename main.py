47b55e380a2573ad5a98c8f1734edf889d0d1403
Hafiz <issahhafiz@gmail.com>
2025-04-29 15:55:54 -0600

    initiate project

---
+++ main.py
@@ -0,0 +1,11 @@
+"""example-repo
+A simple example repository for demonstration purposes.
+"""
+
+def main():
+    """Execute the script."""
+    print("Hello from example-repo!")
+
+
+if __name__ == "__main__":
+    main()
